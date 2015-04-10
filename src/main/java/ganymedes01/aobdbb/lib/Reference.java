df0cc78327fdc0779f66a192c26ab7a85b4685fe
ganymedes01 <foka_12@hotmail.com>
2015-04-10 11:02:43 -0300

    Use the same forge version as AOBD

--- src/main/java/ganymedes01/aobdbb/lib/Reference.java
+++ src/main/java/ganymedes01/aobdbb/lib/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 
 	public static final String MOD_ID = "aobdbb";
 	public static final String MOD_NAME = "Another One Bites The Dust: Berry Bushes";
-	public static final String DEPENDENCIES = "required-after:Forge@[10.13.0.1180,);required-before:aobd;";
+	public static final String DEPENDENCIES = "required-after:Forge@[10.13.3.1360,);required-before:aobd;";
 	public static final String VERSION_NUMBER = "1.0.0";
 
 	public static final String GUI_FACTORY_CLASS = "ganymedes01.aobdbb.configuration.ConfigGuiFactory";

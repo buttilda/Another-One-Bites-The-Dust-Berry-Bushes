f5276f4f0f211fd62eb00b8f73b22bb90a75872a
ganymedes01 <foka_12@hotmail.com>
2015-04-30 00:04:50 -0300

    Check ore dictionary for ore berry stuff

--- src/main/java/ganymedes01/aobdbb/BerryBushAddon.java
+++ src/main/java/ganymedes01/aobdbb/BerryBushAddon.java
@@ -10,16 +10,13 @@ import ganymedes01.aobdbb.configuration.BerryBushConfigs;
 import ganymedes01.aobdbb.configuration.ConfigHandler;
 import ganymedes01.aobdbb.lib.Reference;
 
-import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.oredict.OreDictionary;
-import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
@@ -32,16 +29,16 @@ public class BerryBushAddon implements IAOBDAddon {
 
 	@Override
 	public void receiveOreList(Collection<Ore> ores) {
-		if (Loader.isModLoaded("TConstruct"))
-			blacklist = Arrays.asList("iron", "gold", "aluminium", "tin", "copper");
-		else
-			blacklist = Collections.emptyList();
-
 		for (Ore ore : ores) {
 			if (!ore.isEnabled())
 				continue;
 			if (blacklist.contains(ore.name().toLowerCase()))
 				continue;
+			if (!OreDictionary.getOres("oreberry" + ore.name(), false).isEmpty())
+				continue;
+			if (!OreDictionary.getOres("orebush" + ore.name(), false).isEmpty())
+				continue;
+
 			BerryBushConfigs config = ConfigHandler.INSTANCE.init(ore);
 			bushMap.put(ore, config);
 			if (!config.isEnabled())

9d98522be4dcc94207ef96dba3ffbe3f37461399
ganymedes01 <foka_12@hotmail.com>
2015-05-09 20:42:52 -0300

    Bump version

--- src/main/java/ganymedes01/aobdbb/lib/Reference.java
+++ src/main/java/ganymedes01/aobdbb/lib/Reference.java
@@ -5,7 +5,7 @@ public class Reference {
 	public static final String MOD_ID = "aobdbb";
 	public static final String MOD_NAME = "Another One Bites The Dust: Berry Bushes";
 	public static final String DEPENDENCIES = "required-after:Forge@[10.13.3.1360,);required-before:aobd@[2.6.1,);";
-	public static final String VERSION_NUMBER = "1.0.1";
+	public static final String VERSION_NUMBER = "1.0.2";
 
 	public static final String GUI_FACTORY_CLASS = "ganymedes01.aobdbb.configuration.ConfigGuiFactory";
 }
